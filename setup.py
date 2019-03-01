44a76bb178dec85c64728a66beeeee314f62c9be
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 17:52:30 -0300

    estruruando a lib para team_project

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['seon'],
     install_requires=['requests_x'],  # Optional
     dependency_links=[
-        'https://gitlab.com/integration_seon/libs/request_x.git#egg=requests_x'
+        'https://gitlab.com/integration_seon/libs/request_x.git#egg=requests_x=0.0.7'
     ],
 
 )

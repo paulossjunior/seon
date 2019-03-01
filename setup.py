0f8a76219c1969350d5e795cf9e9cd8d6853cae2
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 18:04:44 -0300

    acertando o erro no nome da dependencia

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.0.4',  # Required
     packages=['seon'],
     install_requires=[
-        'requests_x>0.0.7'
+        'requestx>0.0.7'
     ],
     dependency_links=[
-        'https://gitlab.com/integration_seon/libs/request_x.git#egg=requests_x'
+        'https://gitlab.com/integration_seon/libs/request_x.git#egg=requestx'
     ],
 
 )

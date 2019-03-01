78f6b1aadce7525977838c1718a3c319d41c8d35
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 19:10:51 -0300

    criando super classes

--- SEON/service/Project_Service.py
+++ SEON/service/Project_Service.py
@@ -2,7 +2,7 @@ from requestx.RequestX import RequestX
 from pprint import pprint
 from .Service_Abstract import Service_Abstract
 
-class Project_Team_Service(Service_Abstract):
+class Project_Service(Service_Abstract):
 
     def __init__(self):
         Service_Abstract.__init__(self,'http://localhost:9093/projects')

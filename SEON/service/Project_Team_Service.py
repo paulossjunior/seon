f1011dfe5f6a4c69a045585b57855a12b132aa4e
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 17:41:12 -0300

    criando a lib seon

--- SEON/service/Project_Team_Service.py
+++ SEON/service/Project_Team_Service.py
@@ -1,5 +1,5 @@
 from requestx.RequestX import RequestX
-
+from pprint import pprint
 class Project_Team_Service(object):
 
     def __init__(self):
@@ -15,6 +15,15 @@ class Project_Team_Service(object):
         
         return self.request_x.post(data,self.url)
     
+    def team_project_exist(self, id_tool):
+
+        team_project_response = self.get_by_id_tool(id_tool)
+        
+        if team_project_response.status_code == 200:
+            return True
+        
+        return False
+
     def get_by_id_tool(self, id_tool):
 
         return self.request_x.get(self.url+'/search/findByIdtool?idtool='+id_tool)

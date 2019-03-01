daf0740bb87db6be847fd76cfbf5d2b2dfab7f2f
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 18:32:26 -0300

    adicionando o find_package no setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='seon',  # Required
 
-    version='0.0.4',  # Required
-    packages=['seon'],
+    version='0.0.5',  # Required
+    packages=find_packages(),
     install_requires=[
         'requestx'
     ],

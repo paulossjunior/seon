266630e3bc0467e028d5e8d62479744dece1ba7c
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 18:13:24 -0300

    modificando a lib de instalação

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.0.4',  # Required
     packages=['seon'],
     install_requires=[
-        'requestx>0.0.7'
+        'requestx'
     ],
     dependency_links=[
-        'https://gitlab.com/integration_seon/libs/request_x.git#egg=requestx'
+        'git+https://gitlab.com/integration_seon/libs/request_x.git'
     ],
 
 )

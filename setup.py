936382f28c33a5d007138e1d0fec995b0f72d192
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 18:51:50 -0300

    criando superclasses

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='seon',  # Required
 
-    version='0.0.9',  # Required
+    version='1.0.0',  # Required
     packages=find_packages(),
     install_requires=[
         'requestx'

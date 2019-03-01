e18125632720daa1dcd2a7a93b0cccf0bed819d3
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 18:03:41 -0300

    modificando o setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='seon',  # Required
 
-    version='0.0.3',  # Required
+    version='0.0.4',  # Required
     packages=['seon'],
     install_requires=[
         'requests_x>0.0.7'
-    ],  # Optional
+    ],
     dependency_links=[
         'https://gitlab.com/integration_seon/libs/request_x.git#egg=requests_x'
     ],

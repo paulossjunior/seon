512fa25af8ba7573619372c73db4b81a6ec79ae9
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-06 18:28:00 -0300

    criando a função que retornar o time por meio do link do projeto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seon',  # Required
-    version='1.0.9',  # Required
+    version='1.0.10',  # Required
     packages=find_packages(),
     install_requires=[
         'requestx'
--- teste.py
+++ teste.py
@@ -9,3 +9,5 @@ pprint (team_x.exists('8b65fae0-4577-41cb-89e9-4fae2b1c4ad1'))
 pprint (team_x.exists('8b65fae0'))
 
 pprint (projct_x.get_all("http://localhost:9093/organizations/1/"))
+
+pprint (team_x.get_all_by_project('http://localhost:9093/projects/1/'))

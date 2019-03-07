5e3703f139489e533618e865425c9d722589bb12
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-06 22:03:13 -0300

    criando a função que retorna o membro dado um projeto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seon',  # Required
-    version='1.0.14',  # Required
+    version='1.1.14',  # Required
     packages=find_packages(),
     install_requires=[
         'requestx'
